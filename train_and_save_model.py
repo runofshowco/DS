7d895d61fc24366342ae3b6b43e250356f66d070
tahsin-protik <tahsinprotik048@gmail.com>
2023-08-02 05:07:50 +0600

    fix

--- train_and_save_model.py
+++ train_and_save_model.py
@@ -65,7 +65,7 @@ def main():
 
     except Exception as e:
         print('--->', e)
-        
+        traceback.print_exc()
         data['user_id_list'].append(user_id)
         data['track_user'][user_id]["status"] = "idle"
         update_data(data)
