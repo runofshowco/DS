a8324578ce10cdd13671b9fe731eaa20a05451b4
tahsin-protik <tahsinprotik048@gmail.com>
2023-08-02 04:31:26 +0600

    fix

--- handle_json.py
+++ handle_json.py
@@ -1,14 +1,24 @@
 import json
+from pathlib import Path
+import os
+import string
 
+tmp = __file__
+tmp = "/".join(tmp.split('/')[0:-1])
+json_path = os.path.join(tmp, 'model_status.json')
+
+print(json_path)
 
 def update_data(data):
-    with open("model_status.json", "w") as f:
+    with open(json_path, "w") as f:
         json.dump(data, f, indent=4)
 
 def get_data():
     try:
-        with open("model_status.json", "r") as f:
+        with open(json_path, "r") as f:
             data = json.load(f)
+        
+        print(data)
         
         if data.get('user_id_list') is None:
             data['user_list'] = []
@@ -20,5 +30,6 @@ def get_data():
     except Exception as e:
         print(e)
         return {"user_id_list": [], "track_user": {}}
+
 
     
