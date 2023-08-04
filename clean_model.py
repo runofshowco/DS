96acbfeabbb14a1dd18d2d7d9944c88bbb2bec21
tahsin-protik <tahsinprotik048@gmail.com>
2023-08-05 02:26:45 +0600

    fix

--- clean_model.py
+++ clean_model.py
@@ -1,7 +1,7 @@
 from handle_json import get_data, update_data
 import os
 PROJECT_DIR = "/".join(str(__file__).split('/')[0:-1])
-UPLOAD_FOLDER = 'data/'
+UPLOAD_FOLDER = f'{PROJECT_DIR}/data/'
 MODEL_NAME = "runwayml/stable-diffusion-v1-5"
 OUTPUT_DIR = os.path.join(PROJECT_DIR,"stable_diffusion_weights")
 WEIGHTS_DIR ="stable_diffusion_weights"
@@ -12,7 +12,7 @@ data = get_data()
 new_user_list = []
 
 for key, x in enumerate(data['track_user']):
-    img_path = os.path.join(app.config['UPLOAD_FOLDER'], key, key)
+    img_path = os.path.join(UPLOAD_FOLDER, key, key)
     l = len(os.listdir(img_path))
 
     if l == 0:
