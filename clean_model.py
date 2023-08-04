4d1411efc3fc493638fe0614c96779d60e018e78
tahsin-protik <tahsinprotik048@gmail.com>
2023-08-05 02:24:30 +0600

    fix

---
+++ clean_model.py
@@ -0,0 +1,24 @@
+from handle_json import get_data, update_data
+import os
+PROJECT_DIR = "/".join(str(__file__).split('/')[0:-1])
+UPLOAD_FOLDER = 'data/'
+MODEL_NAME = "runwayml/stable-diffusion-v1-5"
+OUTPUT_DIR = os.path.join(PROJECT_DIR,"stable_diffusion_weights")
+WEIGHTS_DIR ="stable_diffusion_weights"
+
+
+data = get_data()
+
+new_user_list = []
+
+for key, x in data['track_user']:
+    img_path = os.path.join(app.config['UPLOAD_FOLDER'], key, key)
+    l = len(os.listdir(img_path))
+
+    if l == 0:
+        del x[key]
+    else:
+        new_user_list.append(key)
+
+print(new_user_list)
+print(data)
