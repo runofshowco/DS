f9efea5ac426781f7b01e9e817a3074cef808fda
tahsin <tahsinprotik048@gmail.com>
2024-03-20 21:25:15 +0600

    fix create table

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,6 +11,7 @@ unzip "${BASE_DIR}/person.zip" -d "${BASE_DIR}/data/"
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+flask db init
 flask db migrate -m "Initial migration."
 flask db upgrade
 # python3 "${BASE_DIR}/create_tables.py"
